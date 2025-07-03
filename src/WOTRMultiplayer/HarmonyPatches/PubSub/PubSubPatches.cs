9c33af05cab21784905fdefef38ffc5fb7f2d114
Artem Vasylenko <artemjjke@gmail.com>
2025-07-03 15:18:35 +0300

    dialog hooks

--- src/WOTRMultiplayer/HarmonyPatches/PubSub/PubSubPatches.cs
+++ src/WOTRMultiplayer/HarmonyPatches/PubSub/PubSubPatches.cs
@@ -1,12 +1,18 @@
 ﻿using HarmonyLib;
 using Kingmaker;
+using Kingmaker.AreaLogic.Etudes;
 using Kingmaker.Controllers.Clicks.Handlers;
+using Kingmaker.Controllers.Dialog;
+using Kingmaker.DialogSystem.Blueprints;
 using Kingmaker.EntitySystem.Entities;
 using Kingmaker.GameModes;
 using Kingmaker.PubSubSystem;
+using Kingmaker.UnitLogic.Commands.Base;
+using Kingmaker.UnitLogic.Interaction;
 using Kingmaker.View.MapObjects;
 using Microsoft.Extensions.Logging;
 using WOTRMultiplayer.UI;
+using static Kingmaker.AreaLogic.Capital.CapitalCompanionLogic;
 
 namespace WOTRMultiplayer.HarmonyPatches.PubSub
 {
@@ -123,6 +129,87 @@ namespace WOTRMultiplayer.HarmonyPatches.PubSub
                 Game.Instance.UI.Bark(user, UIStringConsts.GameNotifications.TryLeaveAsAClient, 10f);
                 __result = false;
             }
+        }
+
+        [HarmonyPatch(typeof(DialogOnClick), nameof(DialogOnClick.Interact))]
+        [HarmonyPostfix]
+        public static void DialogOnClick_Interact_Postfix(DialogOnClick __instance, UnitEntityData user, UnitEntityData target, ref UnitCommand.ResultType __result)
+        {
+            if (!Main.Multiplayer.IsActive || __result != UnitCommand.ResultType.Success)
+            {
+                return;
+            }
+
+            var dialogueId = __instance.Dialog.name;
+            var userId = user.UniqueId;
+            var targetId = target.UniqueId;
+
+            Main.GetLogger<PubSubPatches>().LogWarning("DialogOnClick. DialogueId={dialogueId}, UserId={userId}, TargetId={targetId}", dialogueId, userId, targetId);
+        }
+
+
+        [HarmonyPatch(typeof(OverrideDialogInteraction), nameof(OverrideDialogInteraction.Interact))]
+        [HarmonyPostfix]
+        public static void OverrideDialogInteraction_Interact_Postfix(OverrideDialogInteraction __instance, UnitEntityData user, UnitEntityData target, ref UnitCommand.ResultType __result)
+        {
+            if (!Main.Multiplayer.IsActive || __result != UnitCommand.ResultType.Success)
+            {
+                return;
+            }
+
+            var dialogueId = __instance.Dialog.name;
+            var userId = user.UniqueId;
+            var targetId = target.UniqueId;
+
+            Main.GetLogger<PubSubPatches>().LogWarning("OverrideDialogInteraction. DialogueId={dialogueId}, UserId={userId}, TargetId={targetId}", dialogueId, userId, targetId);
+        }
+
+        [HarmonyPatch(typeof(EtudeBracketOverrideDialog), nameof(EtudeBracketOverrideDialog.Interact))]
+        [HarmonyPostfix]
+        public static void EtudeBracketOverrideDialog_Interact_Postfix(EtudeBracketOverrideDialog __instance, UnitEntityData user, UnitEntityData target, ref UnitCommand.ResultType __result)
+        {
+            if (!Main.Multiplayer.IsActive || __result != UnitCommand.ResultType.Success)
+            {
+                return;
+            }
+
+            var dialogueId = __instance.Dialog.Guid.ToString();
+            var userId = user.UniqueId;
+            var targetId = target.UniqueId;
+
+            Main.GetLogger<PubSubPatches>().LogWarning("EtudeBracketOverrideDialog. DialogueId={dialogueId}, UserId={userId}, TargetId={targetId}", dialogueId, userId, targetId);
+        }
+
+        [HarmonyPatch(typeof(SpawnerInteractionDialog), nameof(SpawnerInteractionDialog.Interact))]
+        [HarmonyPostfix]
+        public static void SpawnerInteractionDialog_Interact_Postfix(SpawnerInteractionDialog __instance, UnitEntityData user, UnitEntityData target, ref UnitCommand.ResultType __result)
+        {
+            if (!Main.Multiplayer.IsActive || __result != UnitCommand.ResultType.Success)
+            {
+                return;
+            }
+
+            var dialogueId = __instance.Dialog.name;
+            var userId = user.UniqueId;
+            var targetId = target.UniqueId;
+
+            Main.GetLogger<PubSubPatches>().LogWarning("SpawnerInteractionDialog. DialogueId={dialogueId}, UserId={userId}, TargetId={targetId}", dialogueId, userId, targetId);
+        }
+
+        [HarmonyPatch(typeof(DialogController), nameof(DialogController.StartDialogWithUnit))]
+        [HarmonyPrefix]
+        public static void DialogController_Interact_Postfix(DialogController __instance, BlueprintDialog dialog, UnitEntityData unit, UnitEntityData initiator)
+        {
+            if (!Main.Multiplayer.IsActive)
+            {
+                return;
+            }
+
+            var dialogueId = dialog?.name;
+            var userId = initiator?.UniqueId;
+            var targetId = unit?.UniqueId;
+
+            Main.GetLogger<PubSubPatches>().LogWarning("DialogController. DialogueId={dialogueId}, UserId={userId}, TargetId={targetId}", dialogueId, userId, targetId);
         }
     }
 }
