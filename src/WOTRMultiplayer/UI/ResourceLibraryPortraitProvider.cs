ab83c8f531f0248cb072c9311885291a0386dd5e
Artem Vasylenko <artemjjke@gmail.com>
2025-07-06 04:03:22 +0300

    Update ResourceLibraryPortraitProvider.cs

--- src/WOTRMultiplayer/UI/ResourceLibraryPortraitProvider.cs
+++ src/WOTRMultiplayer/UI/ResourceLibraryPortraitProvider.cs
@@ -8,8 +8,10 @@ namespace WOTRMultiplayer.UI
     public class ResourceLibraryPortraitProvider : IPortraitProvider
     {
         public const string PlaceholderPortrait = "Mask_Portrait";
+        public const string PortraitsBundleName = "portraits";
+
         private readonly ILogger _logger;
-        private ConcurrentDictionary<string, UnityEngine.Sprite> _portraits;
+        private ConcurrentDictionary<string, ConcurrentDictionary<string, UnityEngine.Sprite>> _resources;
 
         public ResourceLibraryPortraitProvider(ILogger<ResourceLibraryPortraitProvider> logger)
         {
@@ -18,10 +20,11 @@ namespace WOTRMultiplayer.UI
 
         public UnityEngine.Sprite GetPortrait(string name)
         {
-            if (!_portraits.TryGetValue(name, out var sprite))
+            _resources.TryGetValue(PortraitsBundleName, out var portraits);
+            if (!portraits.TryGetValue(name, out var sprite))
             {
                 _logger.LogWarning("Unable to find requested portrait. Name={portraitName}", name);
-                _portraits.TryGetValue(PlaceholderPortrait, out sprite);
+                portraits.TryGetValue(PlaceholderPortrait, out sprite);
             }
 
             return sprite;
@@ -29,12 +32,16 @@ namespace WOTRMultiplayer.UI
 
         public void Initialize()
         {
-            _portraits = _portraits ??= LoadAssets();
+            if (_resources == null)
+            {
+                _resources = new ConcurrentDictionary<string, ConcurrentDictionary<string, UnityEngine.Sprite>>();
+                _resources.TryAdd(PortraitsBundleName, LoadAssets(PortraitsBundleName));
+            }
         }
 
-        private ConcurrentDictionary<string, UnityEngine.Sprite> LoadAssets()
+        private ConcurrentDictionary<string, UnityEngine.Sprite> LoadAssets(string bundleName)
         {
-            var bundle = BundlesLoadService.Instance.RequestBundle("portraits");
+            var bundle = BundlesLoadService.Instance.RequestBundle(bundleName);
             // had no success to limit loading
             // note: you can't delete (Object->Destroy or DestroyImmediate) redundant sprites as it causes texture errors later on
             var allPortraits = bundle.LoadAllAssets<UnityEngine.Sprite>();
