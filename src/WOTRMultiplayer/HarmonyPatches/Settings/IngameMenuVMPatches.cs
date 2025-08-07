eff12b395166b5aca111f9d4f38b284cd1d8b064
Artem Vasylenko <artemjjke@gmail.com>
2025-08-07 16:41:14 +0300

    disable changing important settings during the game

---
+++ src/WOTRMultiplayer/HarmonyPatches/Settings/IngameMenuVMPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Kingmaker.UI.MVVM._VM.IngameMenu;
+
+namespace WOTRMultiplayer.HarmonyPatches.Settings
+{
+    [HarmonyPatch]
+    public class IngameMenuVMPatches
+    {
+        [HarmonyPatch(typeof(IngameMenuVM), nameof(IngameMenuVM.SwitchTBM))]
+        [HarmonyPrefix]
+        public static bool IngameMenuVM_SwitchTBM_Prefix()
+        {
+            return !Main.Multiplayer.IsActive;
+        }
+    }
+}
---
+++ src/WOTRMultiplayer/HarmonyPatches/Settings/UISettingsRootPatches.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using Kingmaker.Settings;
+using Kingmaker.UI.SettingsUI;
+
+namespace WOTRMultiplayer.HarmonyPatches.Settings
+{
+    [HarmonyPatch]
+    public class UISettingsManagerPatches
+    {
+        private static readonly HashSet<string> UnmodifiableValues = new(
+            [
+                "GameMainSettingsGroup.AcceleratedMove",
+                "GameMainSettingsGroup.AllowLootingInCombat",
+                "TurnBased.EnableTurnBased",
+                "TurnBased.AutoEndTurn",
+                "TurnBased.AutoStopAfterFirstMoveAction",
+                "TurnBased.TimeScaleInPlayerTurn",
+                "TurnBased.TimeScaleInNonPlayerTurn",
+            ],
+            StringComparer.OrdinalIgnoreCase);
+
+        [HarmonyPatch(typeof(UISettingsManager), nameof(UISettingsManager.Initialize))]
+        [HarmonyPostfix]
+        public static void UISettingsManager_Initialize_Postfix(UISettingsManager __instance)
+        {
+            if (!Main.Multiplayer.IsActive)
+            {
+                return;
+            }
+
+            const string GameAutopauseSettingsGroup = "GameAutopauseSettingsGroup";
+
+            foreach (var settingsGroup in __instance.m_GameSettingsList)
+            {
+                var settingsGroupName = settingsGroup.name;
+                foreach (var settingEntity in settingsGroup.SettingsList)
+                {
+                    var settingName = settingEntity.name;
+                    var fullSettingName = $"{settingsGroupName}.{settingName}";
+                    if (settingsGroupName != GameAutopauseSettingsGroup && !UnmodifiableValues.Contains(fullSettingName))
+                    {
+                        continue;
+                    }
+
+                    var floatValue = FindValueBase<float>(settingEntity.GetType(), settingEntity);
+                    if (floatValue != null)
+                    {
+                        floatValue.ManualModificationLock = true;
+                        continue;
+                    }
+                    var boolValue = FindValueBase<bool>(settingEntity.GetType(), settingEntity);
+                    if (boolValue != null)
+                    {
+                        boolValue.ManualModificationLock = true;
+                        continue;
+                    }
+                    var entityValue = FindValueBase<EntitiesType>(settingEntity.GetType(), settingEntity);
+                    if (entityValue != null)
+                    {
+                        entityValue.ManualModificationLock = true;
+                        continue;
+                    }
+                }
+            }
+        }
+
+        private static UISettingsEntityWithValueBase<T> FindValueBase<T>(Type type, object obj)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            if (type.GenericTypeArguments.Length == 0)
+            {
+                return FindValueBase<T>(type.BaseType, obj);
+            }
+
+            if (typeof(UISettingsEntityWithValueBase<T>).IsAssignableFrom(type))
+            {
+                return (UISettingsEntityWithValueBase<T>)obj;
+            }
+
+            return FindValueBase<T>(type.BaseType, obj);
+        }
+    }
+}
