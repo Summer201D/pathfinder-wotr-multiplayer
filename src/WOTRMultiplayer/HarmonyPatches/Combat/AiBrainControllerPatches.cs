d02428c6bf7663533128ac9a81bd98bed12ce9d1
Artem Vasylenko <artemjjke@gmail.com>
2025-08-12 15:02:32 +0300

    restructure

---
+++ src/WOTRMultiplayer/HarmonyPatches/Combat/AiBrainControllerPatches.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Kingmaker;
+using Kingmaker.AI;
+using Kingmaker.EntitySystem.Entities;
+
+namespace WOTRMultiplayer.HarmonyPatches.Combat
+{
+    [HarmonyPatch]
+    public class AiBrainControllerPatches
+    {
+        [HarmonyPatch(typeof(AiBrainController), nameof(AiBrainController.TickBrain))]
+        [HarmonyPrefix]
+        public static bool AiBrainController_TickBrain_Prefix(AiBrainController __instance, UnitEntityData unit)
+        {
+            if (!Main.Multiplayer.IsActive)
+            {
+                return true;
+            }
+
+            var canContinue = !Main.Multiplayer.IsControlledByPlayers(unit.UniqueId);
+            if (!canContinue && Game.Instance.TurnBasedCombatController.CurrentTurn != null)
+            {
+                // game treats characters without control as AI and tries to skip turn if they are stuck
+                // but in reality those characters are controlled by other players and we are waiting for their actions
+                // I believe this could reworked by using transpiler to replace generic condition 'IsDirectlyControllable' in TurnController.Tick => (Status == TurnStatus.Acting && Rider.Commands.Empty && !Rider.IsDirectlyControllable)
+                // with something smarter, but resetting counters work fine for now
+                Game.Instance.TurnBasedCombatController.CurrentTurn.AIForcedTickCount = 0;
+                Game.Instance.TurnBasedCombatController.CurrentTurn.FramesWaitedForStuckAI = 0;
+                Game.Instance.TurnBasedCombatController.CurrentTurn.TimeWaitedForIdleAI = 0;
+            }
+
+            return canContinue;
+        }
+    }
+}
--- src/WOTRMultiplayer/HarmonyPatches/Combat/TurnBasedCombatPatches.cs
+++ src/WOTRMultiplayer/HarmonyPatches/Combat/TurnBasedCombatPatches.cs
@@ -15,7 +15,7 @@ using Kingmaker.TurnBasedMode;
 using Microsoft.Extensions.Logging;
 using TurnBased.Controllers;
 
-namespace WOTRMultiplayer.HarmonyPatches.TurnBasedCombat
+namespace WOTRMultiplayer.HarmonyPatches.Combat
 {
     [HarmonyPatch]
     public class TurnBasedCombatPatches
@@ -69,30 +69,6 @@ namespace WOTRMultiplayer.HarmonyPatches.TurnBasedCombat
             }
 
             var canContinue = Main.Multiplayer.OnBeforeEndTurn(__instance.Rider.UniqueId);
-            return canContinue;
-        }
-
-        [HarmonyPatch(typeof(AiBrainController), nameof(AiBrainController.TickBrain))]
-        [HarmonyPrefix]
-        public static bool AiBrainController_TickBrain_Prefix(AiBrainController __instance, UnitEntityData unit)
-        {
-            if (!Main.Multiplayer.IsActive)
-            {
-                return true;
-            }
-
-            var canContinue = !Main.Multiplayer.IsControlledByPlayers(unit.UniqueId);
-            if (!canContinue && Game.Instance.TurnBasedCombatController.CurrentTurn != null)
-            {
-                // game treats characters without control as AI and tries to skip turn if they are stuck
-                // but in reality those characters are controlled by other players and we are waiting for their actions
-                // I believe this could reworked by using transpiler to replace generic condition 'IsDirectlyControllable' in TurnController.Tick => (Status == TurnStatus.Acting && Rider.Commands.Empty && !Rider.IsDirectlyControllable)
-                // with something smarter, but resetting counters work fine for now
-                Game.Instance.TurnBasedCombatController.CurrentTurn.AIForcedTickCount = 0;
-                Game.Instance.TurnBasedCombatController.CurrentTurn.FramesWaitedForStuckAI = 0;
-                Game.Instance.TurnBasedCombatController.CurrentTurn.TimeWaitedForIdleAI = 0;
-            }
-
             return canContinue;
         }
 
