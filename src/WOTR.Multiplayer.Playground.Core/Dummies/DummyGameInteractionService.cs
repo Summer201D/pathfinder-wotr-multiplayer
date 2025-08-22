ecb4d70c59d6c24194eaf0b724cf1791233f8837
Artem Vasylenko <artemjjke@gmail.com>
2025-08-22 17:54:15 +0300

    Update DummyGameInteractionService.cs

--- src/WOTR.Multiplayer.Playground.Core/Dummies/DummyGameInteractionService.cs
+++ src/WOTR.Multiplayer.Playground.Core/Dummies/DummyGameInteractionService.cs
@@ -26,9 +26,9 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
 {
     public class DummyGameInteractionService : IGameInteractionService
     {
-        public bool IsPaused { get; set; }
+        public RemoteExecutionContext RemoteContext => null;
 
-        public RemoteExecutionContext RemoteContext { get; }
+        public bool IsPaused => false;
 
         public GameModeType CurrentGameMode => GameModeType.None;
 
@@ -40,9 +40,104 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
 
         public bool CampingAutotuneIterationsStatus => false;
 
-        public int CampingIterationsCount => 0;
+        public int CampingIterationsCount => -1;
 
         public bool IsRandomEncounter => false;
+
+        public void AddCombatText(string text)
+        {
+        }
+
+        public void ApplyGameSettings(NetworkGameSettings networkGameSettings)
+        {
+        }
+
+        public void ApplyInspectionKnowledgeCheck(NetworkInspectionKnowledgeCheck networkInspectionKnowledgeCheck)
+        {
+        }
+
+        public void ApplyPerceptionCheck(NetworkPerceptionCheck networkPerceptionCheck)
+        {
+        }
+
+        public void ClickGroundInCombat(NetworkClick networkClick)
+        {
+        }
+
+        public void ClickMapObject(NetworkClick networkClick)
+        {
+        }
+
+        public void ClickUnit(NetworkClick networkClick)
+        {
+        }
+
+        public void CloseVendorWindow()
+        {
+        }
+
+        public void CollectContainerLoot(NetworkLootContainer networkLootContainer)
+        {
+        }
+
+        public bool CombatTurnHasBeenFinished()
+        {
+            return false;
+        }
+
+        public void CompleteLeveling()
+        {
+        }
+
+        public void DecreaseLevelingAbilityScore(NetworkLevelingAbilityScore networkLevelingAbilityScore)
+        {
+        }
+
+        public void DecreaseLevelingSkillPoint(NetworkLevelingSkillPoint networkLevelingSkillPoint)
+        {
+        }
+
+        public void DropItem(NetworkDropItem networkDropItem)
+        {
+        }
+
+        public void EndTurnBasedCombatTurn()
+        {
+        }
+
+        public void ForgetSpell(NetworkSpellSlot networkSpellSlot)
+        {
+        }
+
+        public NetworkActionsState GetActionsState()
+        {
+            return null;
+        }
+
+        public EntityDataBase GetEntity(string id)
+        {
+            return null;
+        }
+
+        public NetworkEquipmentSlotPosition GetEquipmentSlotPosition(ItemSlot slot)
+        {
+            return null;
+        }
+
+        public NetworkGameSettings GetGameSettings()
+        {
+            return null;
+        }
+
+        public List<NetworkCharacterOwnership> GetPartyPlayers()
+        {
+            return [];
+        }
+
+        public string GetPetOwnerId(string unitId)
+        {
+            return null;
+        }
 
         public string GetSaveGamePath()
         {
@@ -51,26 +146,54 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
             return fullPath;
         }
 
-        public bool IsUnitAI(string unitId)
-        {
-            return true;
-        }
-
-        public List<NetworkCharacterOwnership> GetPartyPlayers()
-        {
-            return [];
-        }
-
         public List<NetworkUnit> GetUnitsInCombat()
         {
             return [];
+        }
+
+        public void IncreaseLevelingAbilityScore(NetworkLevelingAbilityScore networkLevelingAbilityScore)
+        {
+        }
+
+        public void IncreaseLevelingSkillPoint(NetworkLevelingSkillPoint networkLevelingSkillPoint)
+        {
+        }
+
+        public void InteractWithOvertip(NetworkOvertip networkOvertip)
+        {
+        }
+
+        public bool IsSummoned(string unitId)
+        {
+            return false;
+        }
+
+        public bool IsUnitAI(string unitId)
+        {
+            return false;
         }
 
         public void LeaveArea(string areaExitId)
         {
         }
 
+        public void LoadGameFromMainMenu(string savePath)
+        {
+        }
+
+        public void MakeVendorDeal()
+        {
+        }
+
         public void MarkSuggestedDialogAnswers(List<NetworkDialogAnswerSuggestion> networkDialogAnswerSuggestions)
+        {
+        }
+
+        public void MemorizeSpell(NetworkSpellSlot networkSpellSlot)
+        {
+        }
+
+        public void MoveNonCombatCharacter(NetworkCharacterMove networkCharacterMove)
         {
         }
 
@@ -82,7 +205,52 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
         {
         }
 
+        public string QuickLoadGame(string savePath)
+        {
+            return null;
+        }
+
+        public void RemoveLevelingSpell(NetworkLevelingSpell networkLevelingSpell)
+        {
+        }
+
+        public void ResetSuggestedDialogAnswers()
+        {
+        }
+
         public void SelectDialogAnswer(string dialogName, string cueName, string answerName, string manualUnitSelectionId)
+        {
+        }
+
+        public void SelectLevelingClass(string classId)
+        {
+        }
+
+        public void SelectLevelingClassArchetype(string archetypeId)
+        {
+        }
+
+        public void SelectLevelingFeature(NetworkLevelingFeature networkLevelingFeature)
+        {
+        }
+
+        public void SelectLevelingSpell(NetworkLevelingSpell networkLevelingSpell)
+        {
+        }
+
+        public void SetActiveHandEquipmentSet(NetworkActiveHandEquipmentSet networkActiveHandEquipmentSet)
+        {
+        }
+
+        public void SetCampingRoles(List<NetworkCampingRole> networkCampingRoles)
+        {
+        }
+
+        public void SetCampingState(NetworkCampingState networkCampingState)
+        {
+        }
+
+        public void SetCampingUseHealingSpells(bool isOn)
         {
         }
 
@@ -90,126 +258,19 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
         {
         }
 
+        public void SetGroundMoveEveryone()
+        {
+        }
+
+        public void SetRandomEncounterContext(NetworkRandomEncounterContext networkRandomEncounterContext)
+        {
+        }
+
+        public void SetStartRestButtonState(bool isInteractable, int readyPlayersCount, int totalPlayersCount)
+        {
+        }
+
         public void ShowModalMessage(string error)
-        {
-        }
-
-        public Task<bool> StartDialogAsync(string dialogName, string targetUnitId, string initiatorUnitId, string mapObjectId, string speakerKey)
-        {
-            return Task.FromResult(true);
-        }
-
-        public string QuickLoadGame(string savePath)
-        {
-            return "1";
-        }
-
-        public void LoadGameFromMainMenu(string savePath)
-        {
-        }
-
-        public string GetPetOwnerId(string unitId)
-        {
-            return null;
-        }
-
-        public void EndTurnBasedCombatTurn()
-        {
-        }
-
-        public Task UpdateUnitsAsync(List<NetworkUnit> networkUnits)
-        {
-            return Task.CompletedTask;
-        }
-
-        public void ClickUnit(NetworkClick click)
-        {
-        }
-
-        public void ClickGroundInCombat(NetworkClick click)
-        {
-        }
-
-        public bool CombatTurnHasBeenFinished()
-        {
-            return true;
-        }
-
-        public NetworkActionsState GetActionsState()
-        {
-            return null;
-        }
-
-        public void UseAbility(NetworkAbility use)
-        {
-        }
-
-        public void ToggleActivatableAbility(NetworkActivatableAbility toggle)
-        {
-        }
-
-        public void ClickMapObject(NetworkClick click)
-        {
-        }
-
-        public void CollectContainerLoot(NetworkLootContainer container)
-        {
-        }
-
-        public void DropItem(NetworkDropItem dropItem)
-        {
-        }
-
-        public NetworkEquipmentSlotPosition GetEquipmentSlotPosition(ItemSlot slot)
-        {
-            return null;
-        }
-
-        public void UpdateEquipmentSlot(NetworkEquipmentSlot slot)
-        {
-        }
-
-        public void SetActiveHandEquipmentSet(NetworkActiveHandEquipmentSet set)
-        {
-        }
-
-        public void InteractWithOvertip(NetworkOvertip networkOvertip)
-        {
-        }
-
-        public EntityDataBase GetEntity(string id)
-        {
-            return null;
-        }
-
-        public bool IsSummoned(string unitId)
-        {
-            return false;
-        }
-
-        public void ResetSuggestedDialogAnswers()
-        {
-        }
-
-        public void ApplyPerceptionCheck(NetworkPerceptionCheck check)
-        {
-        }
-
-        public void UpdateCombatOrder(List<string> combatOrderUnits)
-        {
-        }
-
-        public List<string> GetUnitsCombatOrder()
-        {
-            return [];
-        }
-
-        public NetworkGameSettings GetGameSettings()
-        {
-            return null;
-        }
-
-        public void ApplyGameSettings(NetworkGameSettings gameSettings)
         {
         }
 
@@ -221,19 +282,12 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
         {
         }
 
-        public void SetCampingUseHealingSpells(bool isOn)
+        public Task<bool> StartDialogAsync(string dialogName, string targetUnitId, string initiatorUnitId, string mapObjectId, string speakerKey)
         {
+            return Task.FromResult(false);
         }
 
-        public void SetCampingState(NetworkCampingState state)
-        {
-        }
-
-        public void SetCampingRoles(List<NetworkCampingRole> roles)
-        {
-        }
-
-        public void SetStartRestButtonState(bool isInteractable, int readyPlayersCount, int totalPlayersCount)
+        public void StartLeveling(string unitId)
         {
         }
 
@@ -241,104 +295,11 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
         {
         }
 
-        public void SetRandomEncounterContext(NetworkRandomEncounterContext context)
-        {
-        }
-
-        public string GetNextUnitTurn()
-        {
-            return null;
-        }
-
-        public void SetNextUnitCombatTurn(string nextUnitTurn)
-        {
-        }
-
-        public void ApplyInspectionKnowledgeCheck(NetworkInspectionKnowledgeCheck check)
-        {
-        }
-
-        public void UpdateIsInCombatStatus()
-        {
-        }
-
-        public void TryInterruptRestBanter(NetworkRestBanter banter)
-        {
-        }
-
         public void StartTurnBasedCombatTurn(string unitId)
         {
         }
 
-        public void SetGroundMoveEveryone()
-        {
-        }
-
-        public void AddCombatText(string text)
-        {
-        }
-
-        public void TransferVendorItem(NetworkVendorItemTransfer transfer)
-        {
-        }
-
-        public void CloseVendorWindow()
-        {
-        }
-
-        public void MakeVendorDeal()
-        {
-        }
-
-        public void ForgetSpell(NetworkSpellSlot slot)
-        {
-        }
-
-        public void MemorizeSpell(NetworkSpellSlot slot)
-        {
-        }
-
-        public void StartLeveling(string unitId)
-        {
-        }
-
-        public void SelectLevelingClassArchetype(string archetypeId)
-        {
-        }
-
-        public void SelectLevelingClass(string classId)
-        {
-        }
-
-        public void UpdateLevelingPhaseControls(bool isEnabled)
-        {
-        }
-
-        public void SwitchLevelingPhase(NetworkLevelingPhase phase)
-        {
-        }
-
-        public void DecreaseLevelingSkillPoint(NetworkLevelingSkillPoint skillPoint)
-        {
-        }
-
-        public void IncreaseLevelingSkillPoint(NetworkLevelingSkillPoint skillPoint)
-        {
-        }
-
-        public void SelectLevelingFeature(NetworkLevelingFeature feature)
-        {
-        }
-
-        public void SelectLevelingSpell(NetworkLevelingSpell spell)
-        {
-        }
-
-        public void RemoveLevelingSpell(NetworkLevelingSpell spell)
-        {
-        }
-
-        public void CompleteLeveling()
+        public void SwitchLevelingPhase(NetworkLevelingPhase networkLevelingPhase)
         {
         }
 
@@ -346,15 +307,36 @@ namespace WOTR.Multiplayer.Playground.Core.Dummies
         {
         }
 
-        public void DecreaseLevelingAbilityScore(NetworkLevelingAbilityScore abilityScore)
+        public void ToggleActivatableAbility(NetworkActivatableAbility networkActivatableAbility)
         {
         }
 
-        public void IncreaseLevelingAbilityScore(NetworkLevelingAbilityScore abilityScore)
+        public void TransferVendorItem(NetworkVendorItemTransfer networkVendorItemTransfer)
         {
         }
 
-        public void MoveNonCombatCharacter(NetworkCharacterMove networkCharacterMove)
+        public void TryInterruptRestBanter(NetworkRestBanter networkRestBanter)
+        {
+        }
+
+        public void UpdateEquipmentSlot(NetworkEquipmentSlot networkEquipmentSlot)
+        {
+        }
+
+        public void UpdateIsInCombatStatus()
+        {
+        }
+
+        public void UpdateLevelingPhaseControls(bool isEnabled)
+        {
+        }
+
+        public Task UpdateUnitsAsync(List<NetworkUnit> networkUnits)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void UseAbility(NetworkAbility networkAbility)
         {
         }
     }
