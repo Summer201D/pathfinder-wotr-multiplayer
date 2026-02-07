fe5b25fb28a175273911314ebbb0926a4f918b07
Artem Vasylenko <artemjjke@gmail.com>
2026-02-08 00:34:15 +0200

    sync prev/cutscene roaming

--- src/WOTRMultiplayer/HarmonyPatches/Roaming/RoamingWaypointDataPatches.cs
+++ src/WOTRMultiplayer/HarmonyPatches/Roaming/RoamingWaypointDataPatches.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using HarmonyLib;
+using Kingmaker;
+using Kingmaker.AreaLogic.Cutscenes;
 using Kingmaker.Controllers;
 using Kingmaker.View.Roaming;
 using Microsoft.Extensions.Logging;
@@ -10,6 +12,70 @@ namespace WOTRMultiplayer.HarmonyPatches.Roaming
     [HarmonyPatch]
     public class RoamingWaypointDataPatches
     {
+        [HarmonyPatch(typeof(RoamingWaypointData), nameof(RoamingWaypointData.SelectCutscene))]
+        [HarmonyPrefix]
+        public static bool RoamingWaypointData_SelectCutscene_Prefix(RoamingWaypointData __instance, ref Cutscene __result)
+        {
+            if (!Main.Multiplayer.IsActive)
+            {
+                return true;
+            }
+
+            try
+            {
+                var maxExclusive = __instance.WaypointView.IdleCutscenes.Count;
+                if (maxExclusive == 0)
+                {
+                    return false;
+                }
+
+                var identifier = $"{nameof(RoamingWaypointData)}:{nameof(RoamingWaypointData.SelectCutscene)}:{Game.Instance.CurrentlyLoadedArea.name}:{__instance.UniqueId}";
+                int index = Main.Multiplayer.ValueGenerator.Range(SeedLifetime.Area, identifier, 0, maxExclusive);
+                var cutscene = __instance.WaypointView.IdleCutscenes[index];
+                __result = cutscene.Get();
+
+                Main.GetLogger<RoamingWaypointDataPatches>().LogDebug("Selected cutscene. Identifier={Identifier}, Name={Name}, MaxExclusive={MaxExclusive}", identifier, __result?.name, maxExclusive);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Main.GetLogger<RoamingWaypointDataPatches>().LogError(ex, "Unable to select next cutscene");
+                throw;
+            }
+        }
+
+        [HarmonyPatch(typeof(RoamingWaypointData), nameof(RoamingWaypointData.SelectPrevPoint))]
+        [HarmonyPrefix]
+        public static bool RoamingWaypointData_SelectPrevPoint_Prefix(RoamingWaypointData __instance, ref IRoamingPoint __result)
+        {
+            if (!Main.Multiplayer.IsActive)
+            {
+                return true;
+            }
+
+            try
+            {
+                var maxExclusive = __instance.WaypointView.PrevWaypoints.Count;
+                if (maxExclusive == 0)
+                {
+                    return false;
+                }
+
+                var identifier = $"{nameof(RoamingWaypointData)}:{nameof(RoamingWaypointData.SelectPrevPoint)}:{Game.Instance.CurrentlyLoadedArea.name}:{__instance.UniqueId}";
+                int index = Main.Multiplayer.ValueGenerator.Range(SeedLifetime.Area, identifier, 0, maxExclusive);
+                var waypoint = __instance.WaypointView.PrevWaypoints[index];
+                __result = waypoint.WaypointData;
+
+                Main.GetLogger<RoamingWaypointDataPatches>().LogDebug("Selected previous waypoint. Identifier={Identifier}, Position={Position}, MaxExclusive={MaxExclusive}", identifier, __result?.Position, maxExclusive);
+                return false;
+            }
+            catch (Exception ex)
+            {
+                Main.GetLogger<RoamingWaypointDataPatches>().LogError(ex, "Unable to select previous roaming waypoint");
+                throw;
+            }
+        }
+
         [HarmonyPatch(typeof(RoamingWaypointData), nameof(RoamingWaypointData.SelectNextPoint))]
         [HarmonyPrefix]
         public static bool RoamingWaypointData_SelectNextPoint_Prefix(RoamingWaypointData __instance, ref IRoamingPoint __result)
@@ -21,18 +87,18 @@ namespace WOTRMultiplayer.HarmonyPatches.Roaming
 
             try
             {
-                var maxRange = __instance.WaypointView.NextWaypoints.Count;
-                if (maxRange == 0)
+                var maxExclusive = __instance.WaypointView.NextWaypoints.Count;
+                if (maxExclusive == 0)
                 {
                     return false;
                 }
 
-                var uniqueId = $"{__instance.UniqueId}:{nameof(RoamingWaypointData.SelectNextPoint)}";
-                int nextWaypointIndex = Main.Multiplayer.ValueGenerator.Range(SeedLifetime.Area, uniqueId, 0, maxRange);
+                var identifier = $"{nameof(RoamingWaypointData)}:{nameof(RoamingWaypointData.SelectNextPoint)}:{Game.Instance.CurrentlyLoadedArea.name}:{__instance.UniqueId}";
+                int nextWaypointIndex = Main.Multiplayer.ValueGenerator.Range(SeedLifetime.Area, identifier, 0, maxExclusive);
                 NextWaypointEntry nextWaypointEntry = __instance.WaypointView.NextWaypoints[nextWaypointIndex];
-                __result = nextWaypointEntry?.Waypoint?.WaypointData;
+                __result = nextWaypointEntry.Waypoint?.WaypointData;
 
-                Main.GetLogger<RoamingWaypointDataPatches>().LogDebug("Selected waypoint. Id={Id}, Position={Position}", uniqueId, __result?.Position);
+                Main.GetLogger<RoamingWaypointDataPatches>().LogDebug("Selected next waypoint. Identifier={Identifier}, Position={Position}, MaxExclusive={MaxExclusive}", identifier, __result?.Position, maxExclusive);
                 return false;
             }
             catch (Exception ex)
@@ -53,11 +119,11 @@ namespace WOTRMultiplayer.HarmonyPatches.Roaming
 
             try
             {
-                var uniqueId = $"{__instance.UniqueId}:{nameof(RoamingWaypointData.SelectIdleTime)}";
-                float idleTime = Main.Multiplayer.ValueGenerator.Range(SeedLifetime.Area, uniqueId, __instance.WaypointView.MinIdleTime, __instance.WaypointView.MaxIdleTime);
+                var identifier = $"{nameof(RoamingWaypointData)}:{nameof(RoamingWaypointData.SelectIdleTime)}:{Game.Instance.CurrentlyLoadedArea.name}:{__instance.UniqueId}";
+                float idleTime = Main.Multiplayer.ValueGenerator.Range(SeedLifetime.Area, identifier, __instance.WaypointView.MinIdleTime, __instance.WaypointView.MaxIdleTime);
                 __result = idleTime.Seconds();
 
-                Main.GetLogger<RoamingWaypointDataPatches>().LogDebug("Selected idle time. Id={Id}, RawTime={RawTime}, Time={Time}, MinTimeRange={MinTimeRange}, MaxTimeRange={MaxTimeRange}", uniqueId, idleTime, __result, __instance.WaypointView.MinIdleTime, __instance.WaypointView.MaxIdleTime);
+                Main.GetLogger<RoamingWaypointDataPatches>().LogDebug("Selected idle time. Identifier={Identifier}, RawTime={RawTime}, Time={Time}, MinTimeRange={MinTimeRange}, MaxTimeRange={MaxTimeRange}", identifier, idleTime, __result, __instance.WaypointView.MinIdleTime, __instance.WaypointView.MaxIdleTime);
                 return false;
             }
             catch (Exception ex)
