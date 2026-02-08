36973cd0975002fd5c42a277a317c1781d39bb04
Artem Vasylenko <artemjjke@gmail.com>
2026-02-08 22:02:27 +0200

    log contextvaluehelper dicerolls in combat

---
+++ src/WOTRMultiplayer/Config/Mapping/GameProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Kingmaker.UnitLogic.Abilities;
+using WOTRMultiplayer.Entities.Combat;
+
+namespace WOTRMultiplayer.Config.Mapping
+{
+    public class GameProfile : Profile
+    {
+        public GameProfile()
+        {
+            CreateMap<AbilityData, NetworkAbility>().ConstructUsing(a => CreateNetworkAbility(a));
+        }
+
+        private NetworkAbility CreateNetworkAbility(AbilityData abilityData)
+        {
+            if (abilityData == null)
+            {
+                return null;
+            }
+            abilityData.SpellLevel
+            var ability = new NetworkAbility
+            {
+                Id = abilityData.UniqueId,
+                Name = abilityData.NameForAcronym
+                SpellbookId = abilityData.Spellbook?.Blueprint.Name.Key,
+                ConvertedFromId = abilityData.ConvertedFrom?.UniqueId,
+            };
+            return ability;
+        }
+    }
+}
---
+++ src/WOTRMultiplayer/HarmonyPatches/Combat/ContextValueHelperPatches.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Kingmaker.RuleSystem.Rules;
+using Kingmaker.UnitLogic.Mechanics;
+using Microsoft.Extensions.Logging;
+
+namespace WOTRMultiplayer.HarmonyPatches.Combat
+{
+    [HarmonyPatch]
+    public class ContextValueHelperPatches
+    {
+        [HarmonyPatch(typeof(ContextValueHelper), nameof(ContextValueHelper.CalculateDiceValue))]
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> ContextValueHelper_CalculateDiceValue_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var target = PatchesUtils.GetTranspilerTarget(MethodBase.GetCurrentMethod());
+            var lookFor = AccessTools.PropertyGetter(typeof(RuleRollDice), nameof(RuleRollDice.Result));
+            var replaceWith = AccessTools.Method(typeof(ContextValueHelperPatches), nameof(ContextValueHelperPatches.RollDice));
+            var matcher = new CodeMatcher(instructions);
+            var match = matcher.SearchForward(x => x.Calls(lookFor));
+            if (match.IsInvalid)
+            {
+                Main.GetLogger<ContextValueHelperPatches>().LogError("Transpiler has not been applied. Target={Target}", target);
+                return instructions;
+            }
+
+            var newInstructions = new List<CodeInstruction>()
+            {
+                new(OpCodes.Call, replaceWith),
+            };
+            match = match.Advance(-1).RemoveInstructions(2).Insert(newInstructions);
+            Main.GetLogger<ContextValueHelperPatches>().LogInformation("Transpiler has been applied. Target={Target}", target);
+            return matcher.Instructions();
+        }
+
+        private static int RollDice(MechanicsContext context, RuleRollDice ruleRollDice)
+        {
+            if (Main.Multiplayer.IsActive && Main.Multiplayer.IsInCombat)
+            {
+                Main.GetLogger<ContextValueHelperPatches>().LogWarning("Name={Name}, UnitId={UnitId}, TargetUnitId={TargetUnitId}, TargetPoint={TargetPoint}, AbilityBlueprintId={AbilityBlueprintId}, AbilityName={AbilityName}, ItemName={ItemName}, DiceRolls={DiceRolls}, Dice={Dice}",
+                    context.NameForAcronym, context.MaybeOwner?.UniqueId, context.MainTarget?.Unit, context.MainTarget?.Point, context.SourceAbility?.AssetGuid.ToString(), context.SourceAbility?.NameForAcronym, context.SourceItem?.NameForAcronym, ruleRollDice.DiceFormula.Rolls, ruleRollDice.DiceFormula.Dice);
+            }
+
+            return context.TriggerRule(ruleRollDice).Result;
+        }
+    }
+}
