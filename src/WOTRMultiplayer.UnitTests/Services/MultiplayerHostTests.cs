071eae109be497586b3048b135926b9f7af794ae
Artem Vasylenko <artemjjke@gmail.com>
2026-05-22 00:12:30 +0300

    fix test

--- src/WOTRMultiplayer.UnitTests/Services/MultiplayerHostTests.cs
+++ src/WOTRMultiplayer.UnitTests/Services/MultiplayerHostTests.cs
@@ -92,14 +92,15 @@ namespace WOTRMultiplayer.UnitTests.Services
             // Arrange
             var startUp = new NetworkGameStartUp() { SavePath = Guid.NewGuid().ToString(), IsNewGameSequence = false };
             var gameId = Guid.NewGuid().ToString();
-            var settings = new NetworkMultiplayerSettings { Host = "*", HostPortRangeStart = 123, HostPortRangeEnd = 1234, NetworkAwaiterTimeout = TimeSpan.FromMinutes(1) };
+            var settings = new NetworkMultiplayerSettings { Host = "*", UseIPv6 = true, HostPortRangeStart = 123, HostPortRangeEnd = 1234, NetworkAwaiterTimeout = TimeSpan.FromMinutes(1) };
             A.CallTo(() => _multiplayerSettingsProvider.GetSettings()).Returns(settings);
 
             // Act
             _multiplayerHost.Create(gameId, startUp);
 
             // Assert
-            A.CallTo(() => _networkServer.Start(settings.Host, true, settings.HostPortRangeStart, settings.HostPortRangeEnd, settings.NetworkAwaiterTimeout)).MustHaveHappenedOnceExactly();
+            A.CallTo(() => _networkServer.Start(settings.Host, true, settings.HostPortRangeStart, settings.HostPortRangeEnd, settings.NetworkAwaiterTimeout))
+                .MustHaveHappenedOnceExactly();
         }
 
         [Test]
